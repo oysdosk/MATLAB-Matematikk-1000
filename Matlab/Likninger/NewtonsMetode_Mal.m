f23a2563d7d3e2e43d626b86b450ceebfc1911bf
oysdosk <69964308+oysdosk@users.noreply.github.com>
2021-05-28 13:26:29 +0200

    Lagt til filer i Likninger

---
+++ Matlab/Likninger/NewtonsMetode_Mal.m
@@ -0,0 +1,36 @@
+% Skript som estimerar ei løysing av likninga f(x)=0
+% Skriptet kjører til man får den presisjonen man ønsker.
+% Input er start-verdien x0, funksjonsuttrykket f(x), 
+% den deriverte av funksjonen, f'(x), og presisjonen Pres.
+
+% Startverdi
+x0=-0;
+
+% Funksjon
+funk = @(x) exp(x)+x;
+
+% Derivert:
+funkDeriv = @(x) exp(x)+1;
+
+% Presisjon
+Pres=1e-6;
+
+% Initierer x - ny og gammel
+x=x0;
+xGml=x+10;      %Tilfeldig tall
+
+% Iterer så lenge forskjellen mellom ny og gammel x er større
+% enn Pres.
+while abs(x-xGml)>Pres;
+    xGml=x;
+    format long;
+    x=x-funk(x)/funkDeriv(x)
+end
+
+% Utskrift av svaret
+x;
+
+% Plot
+xVektor=-3:1e-2:3;
+plot(xVektor,funk(xVektor))
+grid on;
